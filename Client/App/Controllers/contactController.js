9bdef5597bf9c0c81c02feb2e92e9df5d2d5d779
Yamil Diego <yamildiego@gmail.com>
2019-01-04 08:35:10 +1300

    contacto

--- Client/App/Controllers/contactController.js
+++ Client/App/Controllers/contactController.js
@@ -1,13 +1,17 @@
-app.controller('contactController', function ($scope, $http, Constants, $rootScope, AuthService) {
-    $scope.errors = [];
-    $scope.form = { name: "", email: "", query: "" };
+app.controller('contactController', function ($scope, $http, Constants, $rootScope, AuthService, $timeout, $window) {
 
-    AuthService.checkAuthInside().then(function (response) {
-        $rootScope.$broadcast("connected", response.data.status);
-        $scope.userData = response.data.data;
-    }, function (response) {
-        $rootScope.$broadcast("disconnected", response.data.status);
-    });
+    $scope.initialize = function () {
+        $scope.errors = [];
+        $scope.seconds = 5;
+        $scope.form = { name: "", email: "", query: "" };
+
+        AuthService.checkAuthInside().then(function (response) {
+            $rootScope.$broadcast("connected", response.data.status);
+            $scope.userData = response.data.data;
+        }, function (response) {
+            $rootScope.$broadcast("disconnected", response.data.status);
+        });
+    }
 
     $scope.send = function () {
         $scope.loadForm = true;
@@ -17,6 +21,7 @@ app.controller('contactController', function ($scope, $http, Constants, $rootSco
                 if (response.data.status == 'OK') {
                     $scope.successful = true;
                     $scope.loadForm = false;
+                    $scope.end();
                 }
             }, function onError(response) {
                 $scope.loadForm = false;
@@ -26,4 +31,17 @@ app.controller('contactController', function ($scope, $http, Constants, $rootSco
                     $scope.errors = [$scope.$parent.getTextError(response.data.status)];
             });
     }
+
+    $scope.end = function () {
+        if ($scope.seconds == 0)
+            $window.location.href = Constants.FRONTURL + '#!/';
+        else {
+            $scope.seconds = $scope.seconds - 1;
+            $timeout(function () {
+                $scope.end();
+            }, 1000);
+        }
+    }
+
+    $scope.initialize();
 });
